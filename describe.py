10f9c2c7867f64ceeb9d2b215bc0f3a01ecd4c00
Benoit Georges <benoit.georges35@gmail.com>
2020-01-23 17:20:16 +0100

    median ready

--- describe.py
+++ describe.py
@@ -7,6 +7,39 @@ import math
 def open_csv(file):
     return pd.read_csv(file)
 
+
+def Quartile3(sortedLst):
+    len_ = len(sortedLst) - 1
+    
+    if (len(sortedLst) % 2 == 1):
+        Q3 = sortedLst[len_ / 4]
+    else:
+        Q3 = sortedLst[len_ / 4] + ((sortedLst[(len_ / 4) + 1] - sortedLst[len_ / 2]) * (3 / 4))
+    return Q3
+
+        
+def Quartile1(sortedLst):
+    # print(sortedLst)
+    len_ = len(sortedLst)
+    
+    if (len_ % 2 == 1):
+        Q1 = sortedLst[int(len_ / 4)]
+    else:
+        print('pair')
+        Q1 = sortedLst[int(len_ / 4)] + ((sortedLst[int((len_ / 4) + 1)] - sortedLst[int(len_ / 4)]) * (3 / 4))
+    print(Q1)
+    return Q1
+
+
+def Median(sortedLst):
+    len_ = int(len(sortedLst) - 1)
+    if (len(sortedLst) % 2 == 1):
+        median = sortedLst[int(len_ / 2)]
+    else:
+        median = (sortedLst[int(len_ / 2)] + sortedLst[int((len_ / 2) + 1)]) / 2
+    return median
+
+
 if __name__ == '__main__':
     if (len(sys.argv[1:]) == 1):
         datas = open_csv(sys.argv[1])
@@ -16,13 +49,13 @@ if __name__ == '__main__':
         mean = []
         std = []
         min_ = []
-        quantile1 = []
+        Q1 = []
         median = []
-        quantile3 = []
+        Q3 = []
         # max_ []
         
         for name in datas:
-            print(name)
+            # print(name)
             col = [x for x in datas[name].tolist() if str(x) != 'nan']
             # print(col)
             # count.append(len(col))
@@ -30,24 +63,14 @@ if __name__ == '__main__':
             # std.append(math.sqrt(sum([(x - mean[-1]) ** 2 for x in col]) / (count[-1] - 1)))
             # min_.append(min(col))
             sortedLst = sorted(col)            
-            if (lstLen % 2 == 1):
-                median.append(sortedLst[int((len(col) - 1) / 2)])
-            else:
-                print((sortedLst[int((len(col) - 1) / 2)] + sortedLst[int((len(col) - 1) / 2) + 1]) / 2)
-            # if len(sort) % 2 == 1:
-            #   median.append( sort[int((len(sort)) / 2)] )
-            # else:
-            #     median.append(sort[int((len(sort) + 1) / 2)])
-            # print(median)
-            # break;
-            # 1quantile.append()
-            
-            # 3quantile.append()
+            median.append(Median(sortedLst))
+            Q1.append(Quartile1(sortedLst))
+            break;
         # print(count)
         # print(mean)
         # print(std)
         # print(min_)
         # print(median)
-        a = datas['Arithmancy'].median()
-        print(a)
-        print(pd.DataFrame.describe(datas))
+        # a = datas['Arithmancy'].median()
+        # print(a)
+        a = pd.DataFrame.describe(datas)
