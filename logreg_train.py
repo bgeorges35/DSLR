a8dd0caa13ecaa48fcfac8d70c68629fb01ab97e
Benoit Georges <benoit.georges35@gmail.com>
2020-02-02 23:45:50 +0100

    finish v2

--- logreg_train.py
+++ logreg_train.py
@@ -2,12 +2,12 @@ import pandas as pd
 import seaborn as sns;
 import sys
 
-def plot_pair(df):
-    sns.set(style="ticks", color_codes=True)
-    sns.pairplot(df, hue="Hogwarts House")
+def logistic_regression(df):
     
+
 if __name__ == "__main__":
     dataset = pd.read_csv(sys.argv[1], index_col = 'Index')
     marks = dataset[dataset.columns[6:]]
     result = pd.concat([marks, dataset[dataset.columns[0]]], axis=1)
-    plot_pair(result)
+    logistic_regression(result)
+    
